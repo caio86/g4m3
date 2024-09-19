e47177209c21464fff98e69aecee681bcaa5c5ec
caio86 <caioluiz86@gmail.com>
2024-09-19 17:18:25 -0300

    style: format flake.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,28 @@
 {
-  inputs = { nixpkgs.url = "nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs systems
-        (system: function (nixpkgs.legacyPackages.${system}));
-    in {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs systems (system: function (nixpkgs.legacyPackages.${system}));
+    in
+    {
       devShells = forAllSystems (pkgs: {
-        default =
-          pkgs.mkShell { packages = with pkgs; [ nixfmt gcc clang-tools ]; };
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            just
+            nixfmt-rfc-style
+            gcc
+            clang-tools
+          ];
+        };
       });
     };
 }
