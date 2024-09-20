bc9187f421c72078d2652f0e8df4d3c15a9d32d2
caio86 <caioluiz86@gmail.com>
2024-09-20 13:32:43 -0300

    p0032 comecei

---
+++ treino/p0032.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  string planetasStr;
+  vector<int> planetas;
+  int N;
+
+  cin >> N;
+
+  getline(cin, planetasStr);
+}
+
+int main() {
+  cin.tie(0); cout.tie(0);
+  ios::sync_with_stdio(0);
+  solve();
+  return 0;
+}
+
