690aa14ef5bda296315e901ff49d3f4bef6adb2c
caio86 <caioluiz86@gmail.com>
2024-09-23 13:41:09 -0300

    p0111 não passou

---
+++ treino/p0111.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int x;
+  cin >> x;
+  cout << pow(x, 2) << "\n";
+}
+
+int main() {
+  cin.tie(0); cout.tie(0);
+  ios::sync_with_stdio(0);
+  solve();
+  return 0;
+}
+
