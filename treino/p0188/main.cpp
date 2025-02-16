718c589fb3d81b8c56ff081c429472f00fc65fc9
caio86 <caioluiz86@gmail.com>
2025-02-16 13:01:46 -0300

    p0188 passou

---
+++ treino/p0188/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef  LOCAL
+#define D(x) cerr << #x << " = " << x << endl;
+#else
+#define D(x)
+#endif
+
+void solve() {
+  cout << 0 << endl;
+}
+
+int main() {
+  cin.tie(0); cout.tie(0);
+  ios::sync_with_stdio(0);
+  solve();
+  return 0;
+}
+
