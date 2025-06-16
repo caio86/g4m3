df7b7efe5f2efe93c9892489ee38ea6022652f4c
caio86 <caioluiz86@gmail.com>
2025-06-16 00:32:46 -0300

    p0210 passou - bolsa magica

---
+++ treino/p0210/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef  LOCAL
+#define D(x) cerr << #x << " = " << x << endl;
+#else
+#define D(x)
+#endif
+
+void solve() {
+  int c; cin >> c;
+
+  cout << c / 7 << endl;
+}
+
+int main() {
+  cin.tie(0); cout.tie(0);
+  ios::sync_with_stdio(0);
+  solve();
+  return 0;
+}
+
