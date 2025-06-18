ba77cbc60cee15d5d24d8d47e1880293ce156849
caio86 <caioluiz86@gmail.com>
2025-06-18 18:39:55 -0300

    p0022 - funcão linear a5b0

---
+++ treino/p0022/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef  LOCAL
+#define D(x) cerr << #x << " = " << x << endl;
+#else
+#define D(x)
+#endif
+
+void solve() {
+  int x; cin >> x;
+
+  cout << x * 5 << endl;
+}
+
+int main() {
+  cin.tie(0); cout.tie(0);
+  ios::sync_with_stdio(0);
+  solve();
+  return 0;
+}
+
